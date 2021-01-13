9fe983c3a870678d4809d9d50e51d334fba13436
Jacob Feitler <jfeitler@gmail.com>
2021-01-13 11:56:42 -0500

    ready to deploy?

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+
 const app = express();
 
 //this connects the database
 connectDB();
 
 //init middleware
-app.use(express.json({ extended: false }))
+app.use(express.json({ extended: false }));
 
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
-app.get('/', (req,res) => res.send('API running'));
+// server static assets in ProfileEducation
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder to
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
